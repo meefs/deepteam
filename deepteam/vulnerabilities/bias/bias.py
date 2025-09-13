1d53f64af15c120495661fe0c80c61814ab6c0ce
A-Vamshi <vamshi.edustudy@icloud.com>
2025-09-13 17:23:17 +0530

    generating attacks in the vulnerability itself

--- deepteam/vulnerabilities/bias/bias.py
+++ deepteam/vulnerabilities/bias/bias.py
@@ -3,18 +3,20 @@ import inspect
 import asyncio
 
 from deepeval.models import DeepEvalBaseLLM
-from deepeval.metrics.utils import initialize_model
+from deepeval.metrics.utils import initialize_model, trimAndLoadJson
 from deepeval.utils import get_or_create_event_loop
 from deepeval.test_case import LLMTestCase
 
 from deepteam.vulnerabilities import BaseVulnerability
 from deepteam.vulnerabilities.bias import BiasType
 from deepteam.vulnerabilities.utils import validate_vulnerability_types
-from deepteam.metrics import BiasMetric
+from deepteam.metrics import BiasMetric, BaseRedTeamingMetric
 from deepteam.attacks import BaseAttack
 from deepteam.attacks.multi_turn.types import CallbackType
-from deepteam.attacks.attack_simulator import AttackSimulator, SimulatedAttack
+from deepteam.attacks.attack_simulator import SimulatedAttack
+from deepteam.attacks.attack_simulator.schema import SyntheticDataList
 from deepteam.risks import getRiskCategory
+from .template import BiasTemplate
 
 BiasLiteralType = Literal["religion", "politics", "gender", "race"]
 
@@ -22,7 +24,6 @@ class Bias(BaseVulnerability):
     def __init__(
         self,
         purpose: str,
-        model: Optional[Union[str, DeepEvalBaseLLM]] = None,
         async_mode: bool = True,
         verbose_mode: bool = False,
         simulator_model: Optional[Union[str, DeepEvalBaseLLM]] = "gpt-3.5-turbo-0125",
@@ -35,7 +36,6 @@ class Bias(BaseVulnerability):
             self.get_name(), types=types, allowed_type=BiasType
         )
         self.purpose = purpose
-        self.model = model
         self.async_mode = async_mode
         self.verbose_mode = verbose_mode
         self.simulator_model = simulator_model
@@ -50,9 +50,7 @@ class Bias(BaseVulnerability):
         max_concurrent: int = 10,
     ):
         from deepteam.red_teamer.risk_assessment import (
-            construct_risk_assessment_overview,
             RedTeamingTestCase,
-            RiskAssessment,
         )
 
         if self.async_mode:
@@ -65,51 +63,80 @@ class Bias(BaseVulnerability):
                     model_callback=model_callback,
                     attack=attack,
                     attacks_per_vulnerability_type=attacks_per_vulnerability_type,
+                    max_concurrent=max_concurrent,
                 )
             )
         else:
             assert not inspect.iscoroutinefunction(
                 model_callback
             ), "`model_callback` needs to be sync. `async_mode` has been set to False."
-
-        self.simulator_model, _ = initialize_model(self.simulator_model)
-        self.evaluation_model, _ = initialize_model(self.evaluation_model)
-        self.attack_simulator = AttackSimulator(
-            simulator_model=self.simulator_model,
-            purpose=self.purpose,
-            max_concurrent=max_concurrent,
-        )
-
-        self.attack_simulator.model_callback = model_callback
-        simulated_attacks: List[SimulatedAttack] = (
-            self.attack_simulator.simulate(
-                attacks_per_vulnerability_type=attacks_per_vulnerability_type,
-                vulnerabilities=[self],
-                attacks=[attack],
-                ignore_errors=False,
-            )
-        )
-        # Create a mapping of vulnerabilities to attacks
-        vulnerability_type_to_attacks_map: Dict[
-            BiasType, List[SimulatedAttack]
-        ] = {}
-        for simulated_attack in simulated_attacks:
-            if (
-                simulated_attack.vulnerability_type
-                not in vulnerability_type_to_attacks_map
-            ):
-                vulnerability_type_to_attacks_map[
-                    simulated_attack.vulnerability_type
-                ] = [simulated_attack]
-            else:
-                vulnerability_type_to_attacks_map[
-                    simulated_attack.vulnerability_type
-                ].append(simulated_attack)
         
-        red_teaming_test_cases: List[RedTeamingTestCase] = []
+        self.simulator_model, self.using_native_model = initialize_model(self.simulator_model)
+        
+        attack_prompts = self.get_attacks(attacks_per_vulnerability_type)
+        baseline_attacks: List[SimulatedAttack] = []
+
+        for type in self.types:
+            for prompt in attack_prompts[type]:
+                if self.using_native_model:
+                    res, _ = self.simulator_model.generate(
+                        prompt, schema=SyntheticDataList
+                    )
+                    local_attacks = [item.input for item in res.data]
+                else:
+                    try:
+                        res: SyntheticDataList = self.simulator_model.generate(
+                            prompt, schema=SyntheticDataList
+                        )
+                        local_attacks = [item.input for item in res.data]
+                    except TypeError:
+                        res = self.simulator_model.generate(prompt)
+                        data = trimAndLoadJson(res)
+                        local_attacks = [item["input"] for item in data["data"]]
+
+            baseline_attacks.extend([
+                SimulatedAttack(
+                    vulnerability=self.get_name(),
+                    vulnerability_type=type,
+                    input=local_attack,
+                    attack_method=attack.get_name()
+                )
+                for local_attack in local_attacks
+            ])
+
+        for baseline_attack in baseline_attacks:
+            sig = inspect.signature(attack.enhance)
+            if (
+                "simulator_model" in sig.parameters
+                and "model_callback" in sig.parameters
+            ):
+                baseline_attack.input = attack.enhance(
+                    attack=baseline_attack.input,
+                    simulator_model=self.simulator_model,
+                    model_callback=model_callback,
+                )
+            elif "simulator_model" in sig.parameters:
+                baseline_attack.input = attack.enhance(
+                    attack=baseline_attack.input,
+                    simulator_model=self.simulator_model,
+                )
+            elif "model_callback" in sig.parameters:
+                baseline_attack.input = attack.enhance(
+                    attack=baseline_attack.input,
+                    model_callback=model_callback,
+                )
+            else:
+                baseline_attack.input = attack.enhance(attack=baseline_attack.input)
+        
+        simulated_attacks = baseline_attacks
+        
+        self.results: Dict[BiasType, List[RedTeamingTestCase]] = dict()
 
         for vulnerability_type in self.types:
-            for simulated_attack in vulnerability_type_to_attacks_map.get(vulnerability_type, []):
+            for simulated_attack in simulated_attacks:
+
+                if simulated_attack.vulnerability_type != vulnerability_type:
+                    continue
 
                 metric = self._get_metric(
                     vulnerability_type
@@ -137,17 +164,9 @@ class Bias(BaseVulnerability):
                 red_teaming_test_case.score = metric.score
                 red_teaming_test_case.reason = metric.reason
 
-                red_teaming_test_cases.append(red_teaming_test_case)
-            
-        self.risk_assessment = RiskAssessment(
-            overview=construct_risk_assessment_overview(
-                red_teaming_test_cases=red_teaming_test_cases
-            ),
-            test_cases=red_teaming_test_cases,
-        )
+                self.results[vulnerability_type] = self.results.get(vulnerability_type, [])
+                self.results[vulnerability_type].append(red_teaming_test_case)
 
-        return self.risk_assessment
-    
     async def a_assess(
         self,
         model_callback: CallbackType,
@@ -156,105 +175,146 @@ class Bias(BaseVulnerability):
         max_concurrent: int = 10,
     ):
         from deepteam.red_teamer.risk_assessment import (
-            construct_risk_assessment_overview,
             RedTeamingTestCase,
-            RiskAssessment,
         )
 
-        self.simulator_model, _ = initialize_model(self.simulator_model)
-        self.evaluation_model, _ = initialize_model(self.evaluation_model)
-        self.attack_simulator = AttackSimulator(
-            simulator_model=self.simulator_model,
-            purpose=self.purpose,
-            max_concurrent=max_concurrent,
-        )
+        assert inspect.iscoroutinefunction(
+            model_callback
+        ), "`model_callback` must be async when using `a_assess`."
 
-        self.attack_simulator.model_callback = model_callback
-        simulated_attacks: List[SimulatedAttack] = (
-            await self.attack_simulator.a_simulate(
-                attacks_per_vulnerability_type=attacks_per_vulnerability_type,
-                vulnerabilities=[self],
-                attacks=[attack],
-                ignore_errors=False,
-            )
-        )
-
-        # Map vulnerabilities to attacks
-        vulnerability_type_to_attacks_map: Dict[BiasType, List[SimulatedAttack]] = {}
-        for simulated_attack in simulated_attacks:
-            if simulated_attack.vulnerability_type not in vulnerability_type_to_attacks_map:
-                vulnerability_type_to_attacks_map[simulated_attack.vulnerability_type] = [simulated_attack]
-            else:
-                vulnerability_type_to_attacks_map[simulated_attack.vulnerability_type].append(simulated_attack)
+        self.simulator_model, self.using_native_model = initialize_model(self.simulator_model)
+        attack_prompts = self.get_attacks(attacks_per_vulnerability_type)
+        baseline_attacks: List[SimulatedAttack] = []
 
         semaphore = asyncio.Semaphore(max_concurrent)
-        tasks = []
 
-        for vulnerability_type in self.types:
-            for simulated_attack in vulnerability_type_to_attacks_map.get(vulnerability_type, []):
+        async def safe_model_callback(prompt: str):
+            async with semaphore:
+                return await model_callback(prompt)
 
-                metric = self._get_metric(vulnerability_type)
+        for type in self.types:
+            for prompt in attack_prompts[type]:
+                if self.using_native_model:
+                    res, _ = await self.simulator_model.a_generate(
+                        prompt, schema=SyntheticDataList
+                    )
+                    local_attacks = [item.input for item in res.data]
+                else:
+                    try:
+                        res: SyntheticDataList = await self.simulator_model.a_generate(
+                            prompt, schema=SyntheticDataList
+                        )
+                        local_attacks = [item.input for item in res.data]
+                    except TypeError:
+                        res = await self.simulator_model.a_generate(prompt)
+                        data = trimAndLoadJson(res)
+                        local_attacks = [item["input"] for item in data["data"]]
 
-                red_teaming_test_case = RedTeamingTestCase(
-                    vulnerability=simulated_attack.vulnerability,
-                    vulnerability_type=vulnerability_type,
-                    attackMethod=simulated_attack.attack_method,
-                    riskCategory=getRiskCategory(vulnerability_type),
-                    input=simulated_attack.input,
-                    metadata=simulated_attack.metadata,
+            baseline_attacks.extend([
+                SimulatedAttack(
+                    vulnerability=self.get_name(),
+                    vulnerability_type=type,
+                    input=local_attack,
+                    attack_method=attack.get_name()
+                )
+                for local_attack in local_attacks
+            ])
+
+        for baseline_attack in baseline_attacks:
+            sig = inspect.signature(attack.a_enhance)
+            if "simulator_model" in sig.parameters and "model_callback" in sig.parameters:
+                baseline_attack.input = await attack.a_enhance(
+                    attack=baseline_attack.input,
+                    simulator_model=self.simulator_model,
+                    model_callback=model_callback,
+                )
+            elif "simulator_model" in sig.parameters:
+                baseline_attack.input = await attack.a_enhance(
+                    attack=baseline_attack.input,
+                    simulator_model=self.simulator_model,
+                )
+            elif "model_callback" in sig.parameters:
+                baseline_attack.input = await attack.a_enhance(
+                    attack=baseline_attack.input,
+                    model_callback=model_callback,
+                )
+            else:
+                baseline_attack.input = await attack.a_enhance(
+                    attack=baseline_attack.input
                 )
 
-                # If there's a simulation error, record and skip evaluation
-                if simulated_attack.error:
-                    red_teaming_test_case.error = simulated_attack.error
-                    tasks.append(asyncio.sleep(0, result=red_teaming_test_case))  # dummy async task
-                    continue
+        simulated_attacks = baseline_attacks
+        self.results: Dict[BiasType, List[RedTeamingTestCase]] = {}
 
-                # Create a coroutine task for this test case
-                async def run_case(
-                    simulated_attack=simulated_attack,
-                    red_teaming_test_case=red_teaming_test_case,
-                    metric=metric,
-                ):
-                    async with semaphore:
-                        target_output = await model_callback(simulated_attack.input)
-                    red_teaming_test_case.actual_output = target_output
+        async def process_attack(simulated_attack: SimulatedAttack):
+            vulnerability_type = simulated_attack.vulnerability_type
 
-                    test_case = LLMTestCase(
-                        input=simulated_attack.input,
-                        actual_output=target_output,
-                    )
+            metric = self._get_metric(vulnerability_type)
 
-                    await metric.a_measure(test_case)
-                    red_teaming_test_case.score = metric.score
-                    red_teaming_test_case.reason = metric.reason
+            red_teaming_test_case = RedTeamingTestCase(
+                vulnerability=simulated_attack.vulnerability,
+                vulnerability_type=vulnerability_type,
+                attackMethod=simulated_attack.attack_method,
+                riskCategory=getRiskCategory(vulnerability_type),
+                input=simulated_attack.input,
+                metadata=simulated_attack.metadata,
+            )
 
-                    return red_teaming_test_case
+            target_output = await safe_model_callback(simulated_attack.input)
+            red_teaming_test_case.actual_output = target_output
 
-                tasks.append(run_case())
+            test_case = LLMTestCase(
+                input=simulated_attack.input,
+                actual_output=target_output,
+            )
 
-        red_teaming_test_cases_all = await asyncio.gather(*tasks)
-        red_teaming_test_cases = [tc for tc in red_teaming_test_cases_all if tc is not None]
+            metric.measure(test_case)
+            red_teaming_test_case.score = metric.score
+            red_teaming_test_case.reason = metric.reason
 
-        self.risk_assessment = RiskAssessment(
-            overview=construct_risk_assessment_overview(
-                red_teaming_test_cases=red_teaming_test_cases
-            ),
-            test_cases=red_teaming_test_cases,
-        )
+            return vulnerability_type, red_teaming_test_case
 
-        return self.risk_assessment
+        # Run all processing concurrently with bounded concurrency
+        all_tasks = [
+            process_attack(simulated_attack)
+            for simulated_attack in simulated_attacks
+            if simulated_attack.vulnerability_type in self.types
+        ]
+
+        for coro in asyncio.as_completed(all_tasks):
+            vulnerability_type, test_case = await coro
+            self.results.setdefault(vulnerability_type, []).append(test_case)
+
+        return self.results
+
 
     def _get_metric(
             self, 
             type: BiasType,
-        ):
+        ) -> BaseRedTeamingMetric:
         return BiasMetric(
             purpose=self.purpose,
-            model=self.model,
+            model=self.evaluation_model,
             async_mode=self.async_mode,
             verbose_mode=self.verbose_mode
         )
+    
+    def get_attacks(
+            self,
+            attacks_per_vulnerability_type: int = 1,
+        ) -> Dict[BiasType, List[str]]:
+        templates = dict()
+        for type in self.types:
+            templates[type] = templates.get(type, [])
+            templates[type].append(
+                BiasTemplate.generate_baseline_attacks(
+                    type, 
+                    attacks_per_vulnerability_type, 
+                    self.purpose
+                )
+            )
+        
+        return templates
 
     def get_name(self) -> str:
         return "Bias"
