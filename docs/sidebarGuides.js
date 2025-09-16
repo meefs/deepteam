662aad71a0957a34deaa0a7e7de8659871c3fb73
A-Vamshi <vamshi.edustudy@icloud.com>
2025-09-16 17:25:03 +0530

    New custom vulnerability guide

---
+++ docs/sidebarGuides.js
@@ -0,0 +1,12 @@
+module.exports = {
+  guides: [
+        {
+        type: "category",
+        label: "Guides",
+        items: [
+            "guide-custom-vulnerability",
+        ],
+        collapsed: false,
+        },
+    ]
+}
