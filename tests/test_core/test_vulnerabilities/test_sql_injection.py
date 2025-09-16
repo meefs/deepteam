11751fac9df6c86c124681adc924de84903efd0c
A-Vamshi <vamshi.edustudy@icloud.com>
2025-09-16 15:38:04 +0530

    updated tests

--- tests/test_core/test_vulnerabilities/test_sql_injection.py
+++ tests/test_core/test_vulnerabilities/test_sql_injection.py
@@ -5,6 +5,7 @@ from deepteam.vulnerabilities.sql_injection import SQLInjectionType
 
 
 class TestSQLInjection:
+    PURPOSE="purpose"
 
     def test_sql_injection_all_types(self):
         types = [
@@ -12,34 +13,34 @@ class TestSQLInjection:
             "union_based_injection",
             "error_based_injection",
         ]
-        sql_injection = SQLInjection(types=types)
+        sql_injection = SQLInjection(purpose=TestSQLInjection.PURPOSE, types=types)
         assert sorted(type.value for type in sql_injection.types) == sorted(
             types
         )
 
     def test_sql_injection_all_types_default(self):
-        sql_injection = SQLInjection()
+        sql_injection = SQLInjection(purpose=TestSQLInjection.PURPOSE, )
         assert sorted(type.value for type in sql_injection.types) == sorted(
             type.value for type in SQLInjectionType
         )
 
     def test_sql_injection_blind_sql_injection(self):
         types = ["blind_sql_injection"]
-        sql_injection = SQLInjection(types=types)
+        sql_injection = SQLInjection(purpose=TestSQLInjection.PURPOSE, types=types)
         assert sorted(type.value for type in sql_injection.types) == sorted(
             types
         )
 
     def test_sql_injection_union_based_injection(self):
         types = ["union_based_injection"]
-        sql_injection = SQLInjection(types=types)
+        sql_injection = SQLInjection(purpose=TestSQLInjection.PURPOSE, types=types)
         assert sorted(type.value for type in sql_injection.types) == sorted(
             types
         )
 
     def test_sql_injection_error_based_injection(self):
         types = ["error_based_injection"]
-        sql_injection = SQLInjection(types=types)
+        sql_injection = SQLInjection(purpose=TestSQLInjection.PURPOSE, types=types)
         assert sorted(type.value for type in sql_injection.types) == sorted(
             types
         )
@@ -52,4 +53,4 @@ class TestSQLInjection:
             "invalid",
         ]
         with pytest.raises(ValueError):
-            SQLInjection(types=types)
+            SQLInjection(purpose=TestSQLInjection.PURPOSE, types=types)
