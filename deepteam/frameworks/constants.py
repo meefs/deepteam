17b4ffb378516e67e476f457112e2bae24d1b0e3
penguine <jeffreyip@confident-ai.com>
2025-11-21 21:34:03 +0800

    .

--- deepteam/frameworks/constants.py
+++ deepteam/frameworks/constants.py
@@ -1,11 +1,15 @@
+from typing import Dict
 from .aegis.aegis import Aegis
+from .frameworks import AISafetyFramework
 from .beavertails.beavertails import BeaverTails
 from .owasp.owasp import OWASPTop10
 from .nist.nist import NIST
 from .mitre.mitre import MITRE
 
 
-FRAMEWORKS_MAP = {f.name: f for f in [OWASPTop10, NIST, MITRE]}
+FRAMEWORKS_MAP: Dict[str, AISafetyFramework] = {
+    f.name: f for f in [OWASPTop10, NIST, MITRE]
+}
 
 FRAMEWORK_RISK_CATEGORIES_MAP = {
     f.name: f.ALLOWED_TYPES for f in [OWASPTop10, NIST, MITRE]
