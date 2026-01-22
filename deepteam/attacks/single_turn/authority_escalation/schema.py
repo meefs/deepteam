58c8978eb0e4cd6b591d5d736dbe970386449048
A-Vamshi <vamshi.edustudy@icloud.com>
2026-01-22 23:51:29 +0530

    Authority Escalation attack added

---
+++ deepteam/attacks/single_turn/authority_escalation/schema.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class EnhancedAuthorityAttack(BaseModel):
+    authority_role: str
+    input: str
+
+class ComplianceData(BaseModel):
+    non_compliant: bool
+
+class IsAuthorityValid(BaseModel):
+    is_valid_authority: bool
