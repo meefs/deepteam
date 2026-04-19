7bb08ba15bffe2e89ebe78757ffb89c226e4728a
A-Vamshi <vamshi.edustudy@icloud.com>
2026-04-19 16:33:18 +0530

    added EU AI Act framework

--- deepteam/frameworks/constants.py
+++ deepteam/frameworks/constants.py
@@ -8,13 +8,15 @@ from .nist.nist import NIST
 from .mitre.mitre import MITRE
 from .risk_category import RiskCategory
 from .owasp_top_10_agentic.owasp_top_10_agentic import OWASP_ASI_2026
+from .eu_ai_act.eu_ai_act import EUAIAct
 from .owasp.risk_categories import OWASP_CATEGORIES
 from .nist.risk_categories import NIST_CATEGORIES
 from .mitre.risk_categories import MITRE_CATEGORIES
 from .owasp_top_10_agentic.risk_categories import OWASP_ASI_CATEGORIES
+from .eu_ai_act.risk_categories import EU_AI_ACT_CATEGORIES
 
 FRAMEWORKS_MAP: Dict[str, AISafetyFramework] = {
-    f.name: f for f in [OWASPTop10, NIST, MITRE, OWASP_ASI_2026]
+    f.name: f for f in [OWASPTop10, NIST, MITRE, OWASP_ASI_2026, EUAIAct]
 }
 
 DATASET_FRAMEWORKS_MAP = {f.name: f for f in [Aegis, BeaverTails]}
@@ -76,6 +78,12 @@ FRAMEWORK_RISK_CATEGORY_MAPPING: Dict[str, FrameworkInfo] = {
         description=OWASP_ASI_2026.description,
         risk_categories=[
             _serialize_risk_category(rc) for rc in OWASP_ASI_CATEGORIES
+        ],
+    ),
+    EUAIAct.name: FrameworkInfo(
+        description=EUAIAct.description,
+        risk_categories=[
+            _serialize_risk_category(rc) for rc in EU_AI_ACT_CATEGORIES
         ],
     ),
 }
